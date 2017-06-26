3dd1ab212948a42c12cd8704ba2948fe2f92fdb8
Leon Lukashevsky <leon.lukashevsky@gmail.com>
2017-06-26 12:04:05 -0400

    Each resource in matcher modal should have a dropdown for all the matching states

--- src/components/need_resource_matcher/recommended_resources/Resource.js
+++ src/components/need_resource_matcher/recommended_resources/Resource.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Glyphicon } from 'react-bootstrap';
+import { Glyphicon, Dropdown, MenuItem } from 'react-bootstrap';
 import _ from 'lodash';
 
 export default class Resource extends Component {
@@ -7,7 +7,16 @@ export default class Resource extends Component {
     const r = this.props.resource;
     return (
       <li className="resource">
-        <Glyphicon glyph="glyphicon glyphicon-pushpin" onClick={this.matchResource} />
+        <Dropdown id='match-menu' pullRight>
+          <CustomToggle bsRole="toggle">
+            <Glyphicon glyph="pushpin" />
+          </CustomToggle>
+          <Dropdown.Menu className="super-colors">
+            <MenuItem eventKey="1" onClick={this.matchResource}>Match</MenuItem>
+            <MenuItem eventKey="2" onClick={this.matchResourcePending}>Pending</MenuItem>
+            <MenuItem eventKey="3" onClick={this.matchResourceFulfilled}>Fulfilled</MenuItem>
+          </Dropdown.Menu>
+        </Dropdown>
         <h5>{_.capitalize(r.type)}</h5>
         {
           Object.keys(r.details).map((key, i) => {
@@ -35,5 +44,20 @@ export default class Resource extends Component {
   saveMatchState = (pending=false, fulfilled=false) => {
     const p = this.props;
     p.saveMatchState(p.resource.id, pending, fulfilled)
+  }
+}
+
+class CustomToggle extends React.Component {
+  handleClick = (e) => {
+    e.preventDefault();
+    this.props.onClick(e);
+  }
+
+  render() {
+    return (
+      <a href="" onClick={this.handleClick}>
+        {this.props.children}
+      </a>
+    );
   }
 }
